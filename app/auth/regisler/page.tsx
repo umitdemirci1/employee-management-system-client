712becb75edbc291e3974fc75def034962d83d24
umitd111 <umit.demirci1996@gmail.com>
2024-10-19 01:04:37 +0300

    Project init with shadcn ui

---
+++ app/auth/regisler/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+interface RegisterProps {
+    companyMail: string;
+    password: string;
+}
+ 
+const Register: FC<RegisterProps> = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default Register;
